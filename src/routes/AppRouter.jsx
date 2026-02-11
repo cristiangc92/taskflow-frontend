921f67a6a3b9115f0ab232a3b9a845cc6a013c2c
Cristian Cacciolatti <cristiangc92@gmail.com>
2026-02-11 16:28:12 -0300

    Implementa protección de rutas con ProtectedRoute y validación de token

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login"
 import Register from "../pages/Register"
 import Dashboard from "../pages/Dashboard"
 import Layout from "../components/Layout";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 function AppRouter(){
     return(
@@ -11,7 +12,11 @@ function AppRouter(){
                 <Route element={<Layout/>}>
                     <Route path="/" element={<Login/>} />
                     <Route path="/register" element={<Register/>} />
-                    <Route path="/dashboard" element={<Dashboard/>} />
+                    <Route path="/dashboard" element={
+                        <ProtectedRoute>
+                            <Dashboard/>
+                        </ProtectedRoute>
+                    } />                        
                 </Route>
             </Routes>
         </BrowserRouter>
