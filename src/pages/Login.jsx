ccd876eca4c82e9e601c2455696f387783a89b3c
Cristian Cacciolatti <cristiangc92@gmail.com>
2026-02-13 17:52:36 -0300

    Corregir error de sintaxis

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { loginUser } from "../services/AuthService"
+import { loginUser } from "../services/authService"
 import { useAuth } from "../context/AuthContext"
 
 function Login() {
